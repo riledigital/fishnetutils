fafce3adf6db744bf878eb8a08b9c83e4d37a44b
Ri <45131445+rl2999@users.noreply.github.com>
2020-02-17 13:55:42 -0500

    beginning messy refactor

--- R/road-buffer.R
+++ R/road-buffer.R
@@ -1,6 +1,16 @@
-library(tigris)
-library(sf)
+# options(tigris_use_cache = FALSE)
 
+#' make_buffered_road
+#'
+#' @param state_name string, state abbreviation
+#' @param county_name numeric 3 digit county code
+#' @param crs_in numeric, EPSG of the local CRS for the county
+#'
+#' @return SF object of a buffered area around roads in the county
+#' @export
+#'
+#' @examples
+#'
 make_buffered_road <- function(state_name, county_name, crs_in) {
   ## Takes in specifications for roads in a county,
   ## returned the buffered areas
@@ -8,64 +18,40 @@ make_buffered_road <- function(state_name, county_name, crs_in) {
                  county = county_name,
                  class = 'sf') %>%
     st_transform(., crs = crs_in) %>%
-    st_buffer(1500) %>%
+    st_buffer(500) %>%
     st_combine() %>%
     st_union(x = . , by_feature = TRUE)
   return(shape)
 }
-options(tigris_use_cache = FALSE)
 
-water_only <- area_water('CA', county= 'Mariposa', class = 'sf', refresh = TRUE) %>%
-  st_transform(crs=3310) %>% st_simplify(dTolerance = 300)
+water_only <-
+  area_water('CA',
+             county = 'Mariposa',
+             class = 'sf',
+             refresh = TRUE) %>%
+  st_transform(crs = 3310) %>% st_simplify(dTolerance = 300)
 
-plot(water_only)
-
-pts_output <- create_fishnet_clip(
-  make_buffered_road('CA', 'Mariposa', 3310), crs_to = 3310)
-
-points_no_water <- st_difference(pts_output, water_only)
+# plot(water_only)
+#
+# pts_output <- create_fishnet_clip(make_buffered_road('CA', 'Mariposa', 3310), crs_to = 3310)
+#
+#
+# fn_brookline <- create_fishnet_clip(make_buffered_road('MA', 021, 102686), crs_to = 102686)
+# points_no_water <- st_difference(pts_output, water_only)
 
 
 # README -----------------------------------------------------------------------
 # This script takes in a .csv of study areas and outputs geojson files
 # that describe a fishnet, water is clipped out
 
-# PACKAGE SETUP
-library('pacman')
-
-Packages <- c(
-  'tidyverse',
-  'dplyr',
-  'tidyr',
-  'readr',
-  'devtools',
-  'googleway',
-  'rmarkdown',
-  'sf',
-  'purrr',
-  'lwgeom',
-  'tmap',
-  'raster',
-  'tigris',
-  'remotes',
-  'yelp',
-  'spData'
-)
-
-p_load(
-  char = Packages,
-  character.only = TRUE,
-  install = TRUE,
-  update = TRUE
-)
 
 # SOME VARIABLES ---------------------------------------------------------------
 CRS_WGS84 <- 4326
 
-tigris_cache_dir(path = data_cache)
-Sys.getenv('TIGRIS_CACHE_DIR')
+# tigris_cache_dir(path = data_cache)
+# Sys.getenv('TIGRIS_CACHE_DIR')
 
-plot(county_montgomery %>% st_bbox())
+# plot(county_montgomery %>% st_bbox())
 
 fetch_county_geographies <- function(fips_state, fips_county) {
   out <- county_subdivisions(
@@ -95,29 +81,43 @@ create_county_boundaries <- function(statey, countyy) {
 }
 
 
-create_fishnet_clip <- function(inputty, crs_to) {
-  print(paste0('inputty CRS is: ', st_crs(inputty)))
-  ## These need to be projected beforehand.
-  input <- inputty %>% st_transform(x = ., crs = crs_to)
-  fishnet <- input %>% st_bbox() %>%
+#' create_fishnet_clip
+#'
+#' @param county_geometry sf/sfc, county area
+#' @param crs_to numeric, desired output CRS
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_fishnet_clip <- function(county_geometry, crs_to) {
+  print(paste0('county_geometry CRS is: ', st_crs(county_geometry)))
+  input <- county_geometry %>%
+    st_transform(x = ., crs = crs_to)
+  # Cell size can get wonky
+    fishnet <-
+    input %>%
+    st_bbox() %>%
     st_make_grid(x = .,
-                 cellsize = 500,
+                 cellsize = 500*10,
                  what = 'centers')
-  # plot(fishnetty)
-  st_set_crs(value = st_crs(input), x = fishnet)
-  pts_output <- st_intersection(fishnet, input)
-  ## TODO Figrue out why the output is badly projected?
-  return(pts_output)
+  # return(fishnet)
+  st_set_crs(x = fishnet, value = sf::st_crs(input))
+  return(st_intersection(fishnet, input))
 }
 
-fishnets_outputs <- c()
+fn_brookline <- create_fishnet_clip(county_geometry = brookline, crs_to = 102686)
+
+sf::write_sf(fn_brookline, dsn='brookline-fn23.gpkg')
+
+# fishnets_outputs <- c()
 
 process_county <- function(x) {
   print(as.numeric(x['crs_utm']))
   county_bounds <-
     create_county_boundaries(statey = x['fips_state'], countyy = x['fips_county'])
   fishnet_clipped <-
-    create_fishnet_clip(inputty = county_bounds, crs_to = as.numeric(x['crs_utm'])) %>%
+    create_fishnet_clip(county_geometry = county_bounds, crs_to = as.numeric(x['crs_utm'])) %>%
     st_transform(x = ., crs = CRS_WGS84)
   ## Then do the export!
   # append(x = fishnets_outputs, values = fishnet_clipped)
@@ -181,20 +181,18 @@ save_file <-
 
 # Folder Setup ----------------------------------------------------------
 
-jobs <-
-  read_csv(
-    file = file.path(data_in, 'study_areas.csv'),
-    col_types = list(
-      col_character(),
-      col_character(),
-      col_character(),
-      col_character(),
-      col_number(),
-      col_character()
-    )
-  )
-
-# Folder Setup ----------------------------------------------------------
+# jobs <-
+#   read_csv(
+#     file = file.path(data_in, 'study_areas.csv'),
+#     col_types = list(
+#       col_character(),
+#       col_character(),
+#       col_character(),
+#       col_character(),
+#       col_number(),
+#       col_character()
+#     )
+#   )
 
 
 # Run the Data Job ----------------------------------------------------------
