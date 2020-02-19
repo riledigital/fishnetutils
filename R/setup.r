791b038263b11ab3ce6df9c11847950582ff045f
Ri Le <r.le@columbia.edu>
2020-02-19 12:11:43 -0500

    dont need to keep setup if we use package loading

--- R/setup.r
+++ R/setup.r
@@ -1,17 +1,17 @@
-# Package Setup
-install.packages('pacman')
-devtools::install_github('rstudio/packrat')
-library('pacman')
-
-p_load(
-  char = Packages,
-  character.only = TRUE,
-  install = TRUE,
-  update = TRUE
-)
-
-# remotes::install_github("https://github.com/richierocks/yelp")
-
-packrat::snapshot()
-
-options(tigris_use_cache = FALSE)
+# # Package Setup
+# install.packages('pacman')
+# devtools::install_github('rstudio/packrat')
+# library('pacman')
+#
+# p_load(
+#   char = Packages,
+#   character.only = TRUE,
+#   install = TRUE,
+#   update = TRUE
+# )
+#
+# # remotes::install_github("https://github.com/richierocks/yelp")
+#
+# packrat::snapshot()
+#
+# options(tigris_use_cache = FALSE)
